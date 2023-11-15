ab6f1929bb233cab4b69f9aa9f6a577ea32c117b
Sfven <33438968+Sfven@users.noreply.github.com>
2023-11-15 17:41:51 -0600

    Update Login.java

--- Login.java
+++ Login.java
@@ -16,7 +16,7 @@ import java.sql.ResultSet;
 
 public class Login extends Application {
 
-    private static final String DB_URL = "jdbc:mysql://sfven.xyz:10888/users";
+    private static final String DB_URL = "jdbc:mysql://166.113.71.13:10888/users";
     private static final String DB_USER = "app";
     private static final String DB_PASSWORD = "&*Mgw41#7evRnVym6CKmmc2jHoYG0*FX"; // this is a password. dont share it with bad people or they can jack with the database ¯\_(ツ)_/¯
 
