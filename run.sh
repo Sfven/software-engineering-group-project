732d98c7b942df82748cce29571006395375ea6a
Sfven <dat.dat82@gmail.com>
2023-11-29 15:43:32 -0600

    update unix scripts and add prepare_workspace for linux

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+# Run this script to run the program. Make sure you have run prepare_workspace and compiled all objects first.
+
 cd ./gamehub/src/gamehub/
 export CLASSPATH=../../../mysql-connector-j-8.2.0/mysql-connector-j-8.2.0.jar:$CLASSPATH
 java --module-path ../../../javafx-sdk-21.0.1/lib/ --add-modules javafx.controls,javafx.fxml Login.java
