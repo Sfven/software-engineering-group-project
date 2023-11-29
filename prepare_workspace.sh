d2d7316c9c396a04cd363f31c12b7db66cbcebfc
Sfven <dat.dat82@gmail.com>
2023-11-29 15:48:36 -0600

    fix linux script

--- prepare_workspace.sh
+++ prepare_workspace.sh
@@ -2,7 +2,7 @@
 
 # Run this script if you have cloned the repo for the first time. It will download JavaFX.
 
-curl -o jfx.zip https://download2.gluonhq.com/openjfx/21.0.1/openjfx-21.0.1_windows-x64_bin-sdk.zip
-tar -xvf jfx.zip
+curl -o jfx.zip https://download2.gluonhq.com/openjfx/21.0.1/openjfx-21.0.1_linux-x64_bin-sdk.zip
+unzip jfx.zip
 rm jfx.zip
 
