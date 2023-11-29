4a5b096b22157e8cec3080cc72b8f113d39d0c85
Sfven <dat.dat82@gmail.com>
2023-11-29 14:17:55 -0600

    Add compile/run scripts for Unix-based OSs

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd ./gamehub/src/gamehub/
+echo "Compiling Login.java"
+javac --module-path ../../../javafx-sdk-21.0.1/lib/ --add-modules javafx.controls,javafx.fxml Login.java
+echo "Compiling gamehub.java"
+javac --module-path ../../../javafx-sdk-21.0.1/lib/ --add-modules javafx.controls,javafx.fxml gamehub.java
+echo "Compiling wordle.java"
+javac --module-path ../../../javafx-sdk-21.0.1/lib/ --add-modules javafx.controls,javafx.fxml wordle.java
+
